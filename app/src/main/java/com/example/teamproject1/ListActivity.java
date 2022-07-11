685defd1e8e2d13360110a3dbd5e1de9f4850637
acto11 <js23zzang@naver.com>
2022-07-11 15:37:55 +0900

    회원리스트 (수정) list

--- app/src/main/java/com/example/teamproject1/ListActivity.java
+++ app/src/main/java/com/example/teamproject1/ListActivity.java
@@ -12,8 +12,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.core.Tag;
-import com.google.firebase.firestore.auth.User;
 
 import java.util.ArrayList;
 
